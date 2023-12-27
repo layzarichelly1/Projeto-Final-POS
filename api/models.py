83477f4ab309eea1a529a2b09fd26cd5ddfaa8ce
layza <layza.richelly@gmail.com>
2023-12-27 11:58:07 -0300

    falta pouco!!!

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    addres = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+class Todo(models.Model):
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(False)
+
+class Comment(models.Model):
+    postId = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    body = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
+    
+class Post(models.Model):
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Todo, Comment, Post
+from .serializers import UserSerializer, TodoSerializer, CommentSerializer, PostSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    Permite a manipulação de dados de Artistas
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    Permite a manipulação de dados de Albuns
+    """
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Permite a manipulação de dados de Músicas
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    Permite a manipulação de dados de Músicas
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
